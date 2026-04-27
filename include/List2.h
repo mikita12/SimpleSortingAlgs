5ec054c9a3cc59ed6f3f6d9a602ef8754178eba4
mikita <patronikm@gmail.com>
2026-04-27 21:46:59 +0200

    feat(List2): added push back

--- include/List2.h
+++ include/List2.h
@@ -21,9 +21,46 @@ public:
     tail = nullptr;
   }
 
-  void push_back(){
-    if(head == nullptr){
-        Node<T> *node = (Node<T> *)malloc(sizeof(Node<T>));
+  void init() {
+    head = (Node<T> *)malloc(sizeof(Node<T>));
+
+    tail = (Node<T> *)malloc(sizeof(Node<T>));
+
+    head->next = tail;
+
+    tail->prev = head;
+
+    head->prev = nullptr;
+
+    tail->next = nullptr;
+
+    head->data = NULL;
+
+    tail->data = NULL;
+  }
+
+  void push_back(const T &value) {
+
+    if (head == nullptr && tail == nullptr) {
+      init();
+      Node<T> *node = (Node<T> *)malloc(sizeof(Node<T>));
+      tail->prev = node;
+      head->next = node;
+      node->data = value;
+      node->prev = head;
+      node->next = tail;
+      return;
     }
+
+    Node<T> *node = (Node<T> *)malloc(sizeof(Node<T>));
+    node->data = value;
+
+    Node<T> *cp = tail->prev;
+
+    node->prev = cp;
+    node->next = tail;
+
+    cp->next = node;
+    tail->prev = node;
   }
 };
